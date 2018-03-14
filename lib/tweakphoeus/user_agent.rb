20e4f1ec97a1806b37423d90579be8dcbc3f094b
Sergio Rojas <sergiorjsd@gmail.com>
2018-03-14 09:11:31 +0100

    Updating User Agents and Accept-Language

--- lib/tweakphoeus/user_agent.rb
+++ lib/tweakphoeus/user_agent.rb
@@ -10,8 +10,8 @@ module Tweakphoeus
     class << self
       def random(systems: OS_DATA[:types])
         user_agent = "Mozilla/5.0 (#{os[systems.sample]}) #{browser}"
-        firefox_version = user_agent.match(/Firefox\/([0-9\.]+)/)&.[](1)
-        user_agent.gsub!(')', "; rv:#{firefox_version})") if firefox_version
+        firefox_version = user_agent.match(/Firefox\/([0-9\.]+)/)
+        user_agent.gsub!(')', "; rv:#{firefox_version[1]})") if firefox_version.is_a?(MatchData)
         user_agent
       end
 
@@ -21,14 +21,14 @@ module Tweakphoeus
         {
           win: "Windows NT #{%w(5 6 7 8 10).sample + '.' + %w(0 1).sample}",
           linux: "X11; #{["", OS_DATA[:linux_distro].sample+"; "].sample}Linux #{OS_DATA[:arch].sample}",
-          mac: "Macintosh; Intel Mac OS X 10_#{Random.rand(9..12)}_#{Random.rand(0..5)}"
+          mac: "Macintosh; Intel Mac OS X 10_#{Random.rand(11..13)}_#{Random.rand(0..3)}"
         }
       end
 
       def browser
-        chrome_version = "Chrome/5#{Random.rand(0..9)}.0.#{Random.rand(1000..4000)}.#{Random.rand(100..900)}"
+        chrome_version = "Chrome/6#{Random.rand(0..5)}.0.#{Random.rand(1000..4000)}.#{Random.rand(100..900)}"
         webkit_603 = "603.#{Random.rand(0..9)}.#{Random.rand(0..9)}"
-        ["Gecko/20100101 Firefox/#{Random.rand(40..54)}.#{Random.rand(0..9)}",
+        ["Gecko/20100101 Firefox/#{Random.rand(55..59)}.0",
          "AppleWebKit/537.36 (KHTML, like Gecko) #{chrome_version} Safari/537.36",
          "AppleWebKit/#{webkit_603} (KHTML, like Gecko) Version/10.1 #{chrome_version} Safari/#{webkit_603}"
         ].sample
