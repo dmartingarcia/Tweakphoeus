27d0818f895d819d19b252cc0a019c838752bf82
David <david.martin@returnly.com>
2021-08-17 19:55:03 +0200

    adding test coverage for basic stuff that is already covered in typhoeus, but, it's something

--- spec/tweakphoeus_spec.rb
+++ spec/tweakphoeus_spec.rb
@@ -5,7 +5,19 @@ describe Tweakphoeus do
     expect(Tweakphoeus::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  [:get, :post, :put, :delete, :head, :patch, :options].each do |name|
+    describe ".#{name}" do
+      let(:response) { Typhoeus::Request.method(name).call("http://localhost:8080/") }
+
+      it "returns ok" do
+        expect(response.return_code).to eq(:ok)
+      end
+
+      unless name == :head
+        it "makes #{name.to_s.upcase} requests" do
+          expect(response.response_body).to include("#{name.to_s.upcase} /")
+        end
+      end
+    end
   end
 end
